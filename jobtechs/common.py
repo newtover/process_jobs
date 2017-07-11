83409f502790f6eb409ce9b76cb99aadc767f243
newtover <newtover@gmail.com>
2017-07-11 00:34:20 +0000

    applied pylint to the rest of the files

--- jobtechs/common.py
+++ jobtechs/common.py
@@ -1,6 +1,8 @@
+"""Common utility functions used in other modules."""
 from collections import OrderedDict
 
 def parse_headers(text):
+    """Parse a string of headers (copied from Firefox) into a dict used in requests."""
     return OrderedDict(line.split(': ') for line in text.splitlines() if line)
 
 
@@ -14,19 +16,22 @@ Upgrade-Insecure-Requests: 1
 """)
 
 def dump_html(text, filename):
-    with open(filename, 'w') as f1:
-        print(text, file=f1)
+    """Helper to save text into a file."""
+    with open(filename, 'w') as file_:
+        print(text, file=file_)
 
 def skip_comments(lines):
+    """Filter out lines starting with a #."""
     return (line for line in lines if not line.startswith('#'))
 
 def rstrip_lines(lines):
+    """Apply rstrip to every line."""
     return (line.rstrip() for line in lines)
 
 def skip_blanks(lines):
+    """Filter out blank lines."""
     return (line for line in lines if line)
 
 def iter_good_lines(lines):
     """Iterate over rstripped lines with skipped empty and comment lines."""
     return skip_blanks(rstrip_lines(skip_comments(lines)))
-
--- jobtechs/scripts/apply_parser.py
+++ jobtechs/scripts/apply_parser.py
@@ -3,12 +3,12 @@
 Used for easier debugging and visual checks."""
 
 import argparse
-from jobtechs.parser import netloc_to_parser_map, TermsExtractor
+from jobtechs.parser import NETLOC_TO_PARSER_MAP, TermsExtractor
 
 def main():
     # pylint: disable=missing-docstring
     parser = argparse.ArgumentParser()
-    parser.add_argument('parser_netloc', choices=list(netloc_to_parser_map.keys()))
+    parser.add_argument('parser_netloc', choices=list(NETLOC_TO_PARSER_MAP.keys()))
     parser.add_argument(
         'infile', type=argparse.FileType('r'),
         help='An HTML-file we are trying to apply the parser to.')
@@ -18,7 +18,7 @@ def main():
 
     args = parser.parse_args()
 
-    page_parser = netloc_to_parser_map[args.parser_netloc]()
+    page_parser = NETLOC_TO_PARSER_MAP[args.parser_netloc]()
     text = args.infile.read()
     extractor = TermsExtractor(args.techs_file)
     url = ''
